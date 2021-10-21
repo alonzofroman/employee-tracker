472a831b2a122d14fa5bca8df6f3531752364351
alonzofroman <alonzofroman@gmail.com>
2021-10-21 16:30:21 -0700

    Create connection to sql database

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+// Load required libraries
 const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const consoleTable = require('console.table');
 
+// create port
 const PORT = process.envPORT || 3000;
 const app = express();
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'employees_db'
+    },
+    console.log('Connected to employees_db.')
+);
+
